bf4eb83469ac8198ecde144d6cec590fe64b0272
mowhits <117739495+mowhits@users.noreply.github.com>
2026-04-27 08:28:27 +0530

    added some code

--- IAR/main.c
+++ IAR/main.c
@@ -1,5 +1,12 @@
+#include "lpc214x.h"
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
 
-int main()
-{
-  return 0;
-}
+#define MAXN 1000
+
+const uint16_t frow[4] = {697, 770, 852, 941};
+const uint16_t fcol[4]
+
+
